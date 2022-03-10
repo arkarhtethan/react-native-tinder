62eb27d8f48c8bd7eed4de696cf1b31c80a18388
Kaung Myat Han <arkarhtethan@gmail.com>
2022-03-10 14:38:08 +0630

    react-native gesture handler configured

--- App.js
+++ App.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import {StyleSheet, View} from 'react-native';
+import {GestureHandlerRootView} from 'react-native-gesture-handler';
 import users from './assets/data/users';
 import Card from './src/components/Card';
 const App = () => {
   return (
-    <View style={styles.container}>
-      <Card user={users[2]} />
-      <Card user={users[1]} />
-    </View>
+    <GestureHandlerRootView>
+      <View style={styles.container}>
+        <Card user={users[2]} />
+        <Card user={users[1]} />
+      </View>
+    </GestureHandlerRootView>
   );
 };
 const styles = StyleSheet.create({
